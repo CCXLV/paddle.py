1c68a1ed4811c2e2082eb39a0d5c88698c640bd2
ccxlv <abracadabragio12@gmail.com>
2025-04-23 19:38:14 +0400

    Update pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -11,41 +11,4 @@ def get_version():
         raise RuntimeError("Unable to find version string.")
 
 
-def get_requirements():
-    with open("requirements.txt") as f:
-        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
-
-
-setup(
-    name="paddle-sdk",
-    description="A modern Python SDK for Paddle's API with type hints and async support",
-    author="CCXLV",
-    version=get_version(),
-    packages=["paddle"],
-    install_requires=get_requirements(),
-    extras_require={
-        "dev": [
-            "setuptools",
-            "black",
-            "ruff",
-            "pytest",
-            "pytest-cov",
-            "pytest-mock",
-            "pytest-asyncio",
-        ],
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
-    url="https://github.com/CCXLV/paddle-sdk",
-    project_urls={
-        "Bug Tracker": "https://github.com/CCXLV/paddle-sdk/issues",
-        "Source Code": "https://github.com/CCXLV/paddle-sdk",
-    },
-    keywords=["paddle", "api", "wrapper", "sdk", "async", "types"],
-    license="MIT",
-)
+setup(version=get_version())
